8677d2b33a99583e7d0799fc2448b03c09696a9c
Munjogu123 <fredmunjogu@gmail.com>
2024-05-07 17:41:57 +0300

    distributes an archive to your web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-""" distributes an archive to your web servers, using the function do_deploy """
+"""
+distributes an archive to your web servers,
+using the function do_deploy
+"""
 
 
 from fabric.api import run, env, put
 from os.path import exists
 env.hosts = ['54.237.4.65', '18.233.64.247']
 
+
 def do_deploy(archive_path):
     """ distributes an archive to your web servers """
     if exists(archive_path) is False:
         return False
-    try:
+    else:
         file = archive_path.split('/')[-1]
         file_name = file.split('.')[0]
         path = '/data/web_static/releases/'
@@ -23,5 +27,3 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, file_name))
         return True
-    except:
-        return False
